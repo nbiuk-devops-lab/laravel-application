64c06f06957f1fb2bd5a1d862350e9f2fb3c1574
nino.biuk <nino.biuk@pooliestudios.com>
2026-02-10 00:15:12 +0100

    added some more markup and started with adding text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,11 @@
         @vite('resources/css/app.css')
     </head>
     <body class="bg-slate-900 text-white" style="background-image: url('{{ asset('images/background.webp') }}')">
-        <p class="text-4xl">
-            Hello Tailwind 🚀
-        </p>
+        <div class="w-[90%] max-w-7xl mx-auto mt-16 p-10 bg-black/60 rounded-xl">
+            <h1 class="text-4xl">
+                Demo-Seite
+            </h1>
+            <p>Diese Seite </p>
+        </div>
     </body>
 </html>
